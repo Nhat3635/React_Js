f99b72bda5efdfb4109fafd80bba532c21d2622f
Tokary2006 <Email ng k GitHub>
2026-04-08 23:52:20 +0700

    [FE][Client] Sản phẩm - DuocTM

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
